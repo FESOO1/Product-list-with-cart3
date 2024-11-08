f5efab6423c4b3e59a7fcfa5e91fdefaf5c1f938
FES001 <firdavs7794948@gmail.com>
2024-11-08 21:12:42 +0500

    working on it.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const productThemselvesContainer = document.querySelector('.products-themselves');
 
 // DISPLAYING DATA
+
+window.addEventListener('DOMContentLoaded', displayData);
+
+async function displayData() {
+    const response = await fetch('./data.json');
+    const productData = await response.json();
+
+};
