624b2d1488690533f9b3b2e9073b4bb8864899cb
FES001 <firdavs7794948@gmail.com>
2024-11-09 02:19:38 +0500

    working on it.

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ let numberOfProducts = 0;
 
 window.addEventListener('DOMContentLoaded', displayData);
 
-async function displayData() {
+async function displayData(productJson) {
     const response = await fetch('./data.json');
     const productData = await response.json();
 
@@ -35,67 +35,73 @@ async function displayData() {
         productItselfTextName[i].textContent = productData[i].name;
         productItselfTextPrice[i].textContent = '$'  + productData[i].price.toFixed(2);
     };
-};
 
-// BUTTONS
 
-for (let i = 0; i < productItselfAddToCartButton.length; i++) {
-    // PRODUCT ADD TO CART BUTTON
-    let isAddedToCart = false;
-    productItselfAddToCartButton[i].addEventListener('click', () => {
-        isAddedToCart = true;
-        productItselfAddToCartButton[i].style.display = 'none';
-        productItselfIcrementDecrementButtonsContainer[i].style.display = 'flex';
 
-        // NUMBER OF PRODUCTS TEXT
-        numberOfProducts++;
-        cartItselfNumberOfProductsText.textContent = numberOfProducts;
+    // BUTTONS
 
-        // DISPLAYING THE PRODUCTS CONTAINER IN THE CART
-        cartItselfEmptyContainer.style.display = 'none';
-        cartItselfProductContainer.style.display = 'flex';
+    for (let i = 0; i < productItselfAddToCartButton.length; i++) {
+        // PRODUCT ADD TO CART BUTTON
+        let isAddedToCart = false;
+        let productCounter = 1;
+        let totalPriceOfOneProduct = productCounter * productData[i].price;
 
-        cartItselfProductsThemselves.innerHTML += `
-            <div class="cart-itself-product-itself">
-                <div class="cart-itself-product-itself-left">
-                    <h4 class="cart-itself-product-itself-left-text">Classic Tiramisu</h4>
-                    <div class="cart-itself-product-itself-left-inner">
-                        <h5 class="cart-itself-product-itself-left-inner-times-text">1x</h5>
-                        <h5 class="cart-itself-product-itself-left-inner-times-price">$5.50</h5>
-                        <h5 class="cart-itself-product-itself-left-inner-times-overall-price">$5.50</h5>
+        productItselfAddToCartButton[i].addEventListener('click', () => {
+            isAddedToCart = true;
+            productItselfAddToCartButton[i].style.display = 'none';
+            productItselfIcrementDecrementButtonsContainer[i].style.display = 'flex';
+
+            // NUMBER OF PRODUCTS TEXT
+            numberOfProducts++;
+            cartItselfNumberOfProductsText.textContent = numberOfProducts;
+
+            // DISPLAYING THE PRODUCTS CONTAINER IN THE CART
+            cartItselfEmptyContainer.style.display = 'none';
+            cartItselfProductContainer.style.display = 'flex';
+
+            // FETCHING DATA FROM THE JSON FILE
+
+            cartItselfProductsThemselves.innerHTML += `
+                <div class="cart-itself-product-itself">
+                    <div class="cart-itself-product-itself-left">
+                        <h4 class="cart-itself-product-itself-left-text">Classic Tiramisu</h4>
+                        <div class="cart-itself-product-itself-left-inner">
+                            <h5 class="cart-itself-product-itself-left-inner-times-text">${productCounter}x</h5>
+                            <h5 class="cart-itself-product-itself-left-inner-times-price">$${productData[i].price.toFixed(2)}</h5>
+                            <h5 class="cart-itself-product-itself-left-inner-times-overall-price">$${totalPriceOfOneProduct.toFixed(2)}</h5>
+                        </div>
+                    </div>
+                    <div class="cart-itself-product-itself-right">
+                        <button class="cart-itself-product-itself-right-delete-button">
+                            <svg class="cart-itself-product-itself-right-delete-button-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
+                            <path d="M19.0005 4.99988L5.00049 18.9999M5.00049 4.99988L19.0005 18.9999" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        </button>
                     </div>
                 </div>
-                <div class="cart-itself-product-itself-right">
-                    <button class="cart-itself-product-itself-right-delete-button">
-                        <svg class="cart-itself-product-itself-right-delete-button-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
-                        <path d="M19.0005 4.99988L5.00049 18.9999M5.00049 4.99988L19.0005 18.9999" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    </button>
-                </div>
-            </div>
-            <hr class="cart-itself-product-divider">
-        `;
-    });
+                <hr class="cart-itself-product-divider">
+            `;
+        });
 
 
-    // INCREMENT AND DECREMENT BUTTONS
-    let productCounter = 1;
+        // INCREMENT AND DECREMENT BUTTONS
 
-    // DECREMENT BUTTON
-    productItselfDecrementButton[i].addEventListener('click', () => {
-        if (productCounter > 1) {
-            productCounter--;
+        // DECREMENT BUTTON
+        productItselfDecrementButton[i].addEventListener('click', () => {
+            if (productCounter > 1) {
+                productCounter--;
+                productItselfIcrementDecrementOutputText[i].textContent = productCounter;
+            } else {
+                isAddedToCart = false;
+                productItselfAddToCartButton[i].style.display = 'flex';
+                productItselfIcrementDecrementButtonsContainer[i].style.display = 'none';
+            };
+        });
+
+        // INCREMENT BUTTON
+        productItselfIncrementButton[i].addEventListener('click', () => {
+            productCounter++;
             productItselfIcrementDecrementOutputText[i].textContent = productCounter;
-        } else {
-            isAddedToCart = false;
-            productItselfAddToCartButton[i].style.display = 'flex';
-            productItselfIcrementDecrementButtonsContainer[i].style.display = 'none';
-        };
-    });
-
-    // INCREMENT BUTTON
-    productItselfIncrementButton[i].addEventListener('click', () => {
-        productCounter++;
-        productItselfIcrementDecrementOutputText[i].textContent = productCounter;
-    });
+        });
+    };
 };
