5ef99b51c46ab9d81bb972c02ff68d0444e122fd
FES001 <firdavs7794948@gmail.com>
2024-11-08 21:17:01 +0500

    working on it.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const productThemselvesContainer = document.querySelector('.products-themselves');
+const productItself = document.querySelectorAll('.product-itself');
+const productItselfImage = document.querySelectorAll('.product-itself-image-itself');
+const productItselfAddToCartButton = document.querySelectorAll('.product-itself-add-to-cart-button');
+const productItselfIcrementDecrementButtonsContainer = document.querySelector('.product-itself-buttons-increment-decrement-buttons');
+const productItselfDecrementButton = document.querySelectorAll('.product-itself-buttons-decrement-button');
+const productItselfIncrementButton = document.querySelectorAll('.product-itself-buttons-increment-button');
+const productItselfTextCategory = document.querySelectorAll('.product-itself-text-category');
+const productItselfTextName = document.querySelectorAll('.product-itself-text-name');
+const productItselfTextPrice = document.querySelectorAll('.product-itself-text-category');
 
 // DISPLAYING DATA
 
@@ -7,5 +16,6 @@ window.addEventListener('DOMContentLoaded', displayData);
 async function displayData() {
     const response = await fetch('./data.json');
     const productData = await response.json();
+
 
 };
